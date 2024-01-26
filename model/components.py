7bea5bc3ec024704d643084cd087748a49990805
pianwan <pianwan@outlook.com>
2024-01-26 23:16:16 +0100

    back

--- model/components.py
+++ model/components.py
@@ -63,7 +63,7 @@ class LocalConnect(nn.Module):
         # Sum in in_channel and kernel_size dims
         out = (x.unsqueeze(1) * self.weight).sum([2, -1])
         if self.bias is not None:
-            out += torch.tensor(self.bias).to(x.device)
+            out += self.bias
         return out
 
 
